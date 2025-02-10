b62e2a02d66a13671ed1a1a8884b9c06348f8333
Prakhar <pawasthi961@gmail.com>
2025-02-10 18:07:54 +0530

    reverting back to 1.5b for base

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 /bin/ollama serve &
 pid=$!
 sleep 5
-echo "Retrieve deepseek-r1:7b model..."
-ollama pull deepseek-r1:7b
+echo "Retrieve deepseek-r1:1.5b model..."
+ollama pull deepseek-r1:1.5b
 echo "Done!"
 wait $pid
